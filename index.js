2088eb521bf0965091245dfeb450a9122703b5cc
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-06-15 08:49:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This is on me!';
+  }else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if (distance > 2500) {
+    return 'No can do.';
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'ok, sounds good.' : 'No go';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
 }
+  }
+  // Write your code here!
